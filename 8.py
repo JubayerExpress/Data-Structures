99aac7223e662081218017f1b4f7fea00502deec
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-02-09 00:59:27 +0600

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,1 +1,33 @@
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        self.graph[u].append(v)
+
+    def dfs(self, start):
+        visited = set()
+
+        def dfs_util(v):
+            visited.add(v)
+            print(v, end=' ')
+            for neighbor in self.graph.get(v, []):
+                if neighbor not in visited:
+                    dfs_util(neighbor)
+
+        dfs_util(start)
+
+# Test cases
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 0)
+g.add_edge(2, 3)
+g.add_edge(3, 3)
+
+print("DFS starting from vertex 2:")
+g.dfs(2)  # Output: 2 0 1 3
 
