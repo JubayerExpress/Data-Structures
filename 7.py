d36d5bc1042cde93367da896e265daeb6bf50863
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-02-09 00:59:19 +0600

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,1 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            last = self.head
+            while last.next:
+                last = last.next
+            last.next = new_node
+
+    def find_middle(self):
+        slow = self.head
+        fast = self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow.data if slow else None
+
+# Test cases
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.append(4)
+ll.append(5)
+print("Middle element:", ll.find_middle())  # Output: 3
 
