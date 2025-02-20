b9fdb67f81be118bb80af4a33d0cf00bddd9da96
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-02-20 23:32:54 +0600

    Update 11.py

--- 11.py
+++ 11.py
@@ -1,1 +1,19 @@
+class ListNode:
+    def __init__(self, value=0, next=None):
+        self.value = value
+        self.next = next
+
+def reverse_linked_list(head):
+    prev = None
+    current = head
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+    return prev
+
+# Example usage:
+head = ListNode(1, ListNode(2, ListNode(3, ListNode(4))))
+reversed_list = reverse_linked_list(head)
 
