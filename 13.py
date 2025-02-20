daf63ab71ff8271d38973ce9e8a7af22fd35bf06
G M Jubayer Zaman <gmjz.back@gmail.com>
2025-02-20 23:33:21 +0600

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,1 +1,23 @@
 
+class QueueWithTwoStacks:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def enqueue(self, value):
+        self.stack1.append(value)
+
+    def dequeue(self):
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        if self.stack2:
+            return self.stack2.pop()
+        return None
+
+# Example usage:
+queue = QueueWithTwoStacks()
+queue.enqueue(1)
+queue.enqueue(2)
+print(queue.dequeue())  # Output: 1
+print(queue.dequeue())  # Output: 2
